05352eabc2b44a3f867863261618ef03210b40ee
Mrunal Pawshekar <53634915+mrunalpawshekar@users.noreply.github.com>
2020-07-15 14:37:20 +0530

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def about(request):
-    return HttpResponse('welcome to About page ... !')
-
-def homepage(request):
-    return HttpResponse('welcome to homepage... !')
